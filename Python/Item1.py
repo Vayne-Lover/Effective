062d882ef32d9663b463a4ccb3b05dd5a9f9cfec
Vayne-Lover <406378362@qq.com>
2016-12-25 18:18:21 +0800

    Add Item1:Know which version of python you are using.

---
+++ Python/Item1.py
@@ -0,0 +1,9 @@
+#!/usr/local/bin/python
+#Know which version of python you are using.
+#python --version
+#Python 2.7.11
+#python3 --version
+#Python 3.5.2
+import sys
+print sys.version
+print sys.version_info
