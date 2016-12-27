f9bad030fffbf6b50ec5833c4024656c725c7679
Vayne-Lover <406378362@qq.com>
2016-12-27 19:27:13 +0800

    Rename and add item3.

---
+++ Python/item3.py
@@ -0,0 +1,21 @@
+#!/usr/local/bin/python
+# -*- coding:utf-8 -*-
+import sys
+
+def to_unicode(unicode_or_str):
+  if isinstance(unicode_or_str,str):
+    v=unicode_or_str.decode('utf-8')
+  else:
+    v=unicode_or_str
+  return v
+
+def to_str(unicode_or_str):
+  if isinstance(unicode_or_str,unicode):
+    v=unicode_or_str.encode('utf-8')
+  else:
+    v=unicode_or_str
+  return v
+
+str='十大'
+#print to_unicode(str)
+print sys.getdefaultencoding()
