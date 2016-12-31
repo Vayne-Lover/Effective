e3df68ebabc8af14a1101635a3b3fc0f23333445
Vayne-Lover <406378362@qq.com>
2016-12-31 13:52:28 +0800

    Add item6.

---
+++ Python/item6.py
@@ -0,0 +1,13 @@
+#!/usr/local/bin/python
+a=['red','orange','yellow','green','blue','purple']
+odds=a[::2]
+evens=a[1::2]
+print odds
+print evens
+x=b'abcdefg'
+y=x[::-1]
+print y
+c=['a','b','c','d','e','f']
+d=c[::2]
+e=d[1:-1]
+print e
