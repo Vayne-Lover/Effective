9f39c7740d23b115680c36968407cf110029dbee
Vayne-Lover <406378362@qq.com>
2017-01-18 19:08:26 +0800

    Add another way to solve the problem.

--- Python/item14.py
+++ Python/item14.py
@@ -1,23 +1,39 @@
 #!/usr/local/bin/python
-def divide1(x,y):
+#def divide1(x,y):
+#  try:
+#    return True,x/y
+#  except Exception , e:
+#    print e
+#    return False,None
+#
+#x1=5
+#y1=0
+#x2=2
+#y2=1
+#success1,result1= divide1(x1,y1)
+#if not success1:
+#  print "Error"
+#else:
+#  print result1
+#
+#success2,result2= divide1(x2,y2)
+#if not success2:
+#  print "Error"
+#else:
+#  print result2
+
+def divide(x,y):
   try:
-    return True,x/y
-  except Exception , e:
-    print e
-    return False,None
+    return x/y
+  except ValueError , e:
+    raise ValueError('Input value error')
 
-x1=5
-y1=0
-x2=2
-y2=1
-success1,result1= divide1(x1,y1)
-if not success1:
-  print "Error"
+x,y=0,2
+try:
+  result=divide(x,y)
+except ValueError:
+  print 'Value error.'
 else:
-  print result1
+  print result
 
-success2,result2= divide1(x2,y2)
-if not success2:
-  print "Error"
-else:
-  print result2
+
