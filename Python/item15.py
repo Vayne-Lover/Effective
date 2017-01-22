9ce33c3577c2ca281de11e0d6175cc24dc35249c
Vayne-Lover <406378362@qq.com>
2017-01-22 21:39:19 +0800

    Add sort_priority3 for python3.

--- Python/item15.py
+++ Python/item15.py
@@ -1,4 +1,4 @@
-#!/usr/local/bin/python
+# -*- coding: utf-8 -*-
 
 def sort_priority(num,pro):
   res=num[:]
@@ -9,7 +9,22 @@ def sort_priority(num,pro):
   res.sort(key=helper)
   return res
 
-numbers=[2,5,7,4,1,3,8,6]
-group=[2,4,8]
+def sort_priority3(num,pro):
+  found=False
+  def helper(x):
+    nonlocal found
+    if x in pro:
+      found=True
+      return (0,x)
+    return (1,x)
+  num.sort(key=helper)
+  return found
 
-print sort_priority(numbers,group)
+if __name__=='__main__':
+  numbers=[2,5,7,4,1,3,8,6]
+  group=[2,4,8]
+
+  print(sort_priority(numbers,group))
+  print(numbers)
+  print(sort_priority3(numbers,group))
+  print(numbers)
