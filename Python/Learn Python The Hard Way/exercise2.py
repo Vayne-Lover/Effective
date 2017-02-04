3559fe7ff1022f718106c32b39429e2071846b8c
Vayne-Lover <406378362@qq.com>
2017-02-04 20:57:02 +0800

    Add exercise2.

---
+++ Python/Learn Python The Hard Way/exercise2.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+
+# A comments.
+# Ignored by python.
+
+print("I just have code like this!")# Ignored.
+
+# Use comments.
+# print("This won't run.")
+
+print("This will run.")
