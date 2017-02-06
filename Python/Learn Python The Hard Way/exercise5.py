1ecb0119fd3b976504e79cf3f6a2750fe39f5ee2
Vayne-Lover <406378362@qq.com>
2017-02-06 13:50:33 +0800

    Add exercise5.

---
+++ Python/Learn Python The Hard Way/exercise5.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+
+my_name = 'Vayne'
+
+my_age = 25
+
+my_height = 175
+
+my_weight = 64
+
+my_eyes = 'Black'
+
+my_teeth = 'White'
+
+my_hair = 'Black'
+
+print("Let's talk about {}.".format(my_name))
+
+print("He is %d years old." % my_age)
+
+print("He is {0} cm tall,{1} kg.".format(my_height,my_weight))
+
+
