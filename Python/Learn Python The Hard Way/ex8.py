33eae04cc3cc97898cb1bbc5723784953b25156d
Vayne-Lover <406378362@qq.com>
2017-02-09 17:52:19 +0800

    Add exercise8.

---
+++ Python/Learn Python The Hard Way/ex8.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+
+formatter = "%r %r %r %r"
+
+print formatter % (1, 2, 3, 4)
+
+print formatter % ("one", "two", "three", "four")
+
+print formatter % (True, False ,True ,False)
+
+print formatter % (formatter, formatter, formatter, formatter)
+
+print formatter % (
+		   "I had this." ,
+                   "You could type up right.",
+		   "But it didn't sing.",
+		   "So i said bye."
+		  )
