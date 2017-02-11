05428ac692838b0d7d9f9e582f12c2c738a23df9
Vayne-Lover <406378362@qq.com>
2017-02-11 20:42:02 +0800

    Add exercise11.

---
+++ Python/Learn Python The Hard Way/ex11.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+
+print("Please input your name :",end=" ")
+
+name=input()
+
+print("Please input your age :",end=" ")
+
+age=input()
+
+print("Please input your height :",end=" ")
+
+height=input()
+
+print("I'm {0},{1} years old,{2} cm tall.".format(name,age,height))
