cfd30171f42239392c8c80b9ba7c14cad166fc26
Vayne-Lover <406378362@qq.com>
2017-02-11 21:15:12 +0800

    Add python3 edition.

--- Python/Learn Python The Hard Way/ex12.py
+++ Python/Learn Python The Hard Way/ex12.py
@@ -1,10 +1,19 @@
 # -*- coding: utf-8 -*-
+#
+#age = raw_input("How old are you? ")
+#
+#height = raw_input("How tall are you? ")
+#
+#weight = raw_input("How much do you weigh? ")
+#
+#print "So, you're %r old, %r tall and %r heavy." % (
+#    age, height, weight)
 
-age = raw_input("How old are you? ")
+age = input("How old are you? ")
 
-height = raw_input("How tall are you? ")
+height = input("How tall are you? ")
 
-weight = raw_input("How much do you weigh? ")
+weight = input("How much do you weigh? ")
 
-print "So, you're %r old, %r tall and %r heavy." % (
-    age, height, weight)
+print("So, you're {0} old, {1} tall and {2} heavy.".format(
+    age, height, weight))
