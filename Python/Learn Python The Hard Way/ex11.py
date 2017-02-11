5f64864e8d3fc1c5192dd6bf737f6942ef250e4e
Vayne-Lover <406378362@qq.com>
2017-02-11 21:08:49 +0800

    Add python2 exercise.

--- Python/Learn Python The Hard Way/ex11.py
+++ Python/Learn Python The Hard Way/ex11.py
@@ -13,3 +13,18 @@ print("Please input your height :",end=" ")
 height=input()
 
 print("I'm {0},{1} years old,{2} cm tall.".format(name,age,height))
+
+#print "How old are you?",
+#
+#age = raw_input()
+#
+#print "How tall are you?",
+#
+#height =raw_input()
+#
+#print "How much do you weigh?",
+#
+#weight = raw_input()
+#
+#print "So, you're %r old, %r tall and %r heavy." % (
+#    age, height, weight)
