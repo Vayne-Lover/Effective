864aeb56f3a60ab7414fb471421edeeef2a6fd46
Vayne-Lover <406378362@qq.com>
2017-02-11 21:20:53 +0800

    Compare %r and %s for exercise 12.

--- Python/Learn Python The Hard Way/ex11.py
+++ Python/Learn Python The Hard Way/ex11.py
@@ -1,30 +1,30 @@
 # -*- coding: utf-8 -*-
-
-print("Please input your name :",end=" ")
-
-name=input()
-
-print("Please input your age :",end=" ")
-
-age=input()
-
-print("Please input your height :",end=" ")
-
-height=input()
-
-print("I'm {0},{1} years old,{2} cm tall.".format(name,age,height))
-
-#print "How old are you?",
 #
-#age = raw_input()
+#print("Please input your name :",end=" ")
 #
-#print "How tall are you?",
+#name=input()
 #
-#height =raw_input()
+#print("Please input your age :",end=" ")
 #
-#print "How much do you weigh?",
+#age=input()
 #
-#weight = raw_input()
+#print("Please input your height :",end=" ")
 #
-#print "So, you're %r old, %r tall and %r heavy." % (
-#    age, height, weight)
+#height=input()
+#
+#print("I'm {0},{1} years old,{2} cm tall.".format(name,age,height))
+
+print "How old are you?",
+
+age = raw_input()
+
+print "How tall are you?",
+
+height =raw_input()
+
+print "How much do you weigh?",
+
+weight = raw_input()
+
+print "So, you're %s old, %s tall and %s heavy." % (
+    age, height, weight)
--- Python/Learn Python The Hard Way/ex12.py
+++ Python/Learn Python The Hard Way/ex12.py
@@ -1,19 +1,22 @@
 # -*- coding: utf-8 -*-
-#
-#age = raw_input("How old are you? ")
-#
-#height = raw_input("How tall are you? ")
-#
-#weight = raw_input("How much do you weigh? ")
-#
-#print "So, you're %r old, %r tall and %r heavy." % (
-#    age, height, weight)
 
-age = input("How old are you? ")
+age = raw_input("How old are you? ")
 
-height = input("How tall are you? ")
+height = raw_input("How tall are you? ")
 
-weight = input("How much do you weigh? ")
+weight = raw_input("How much do you weigh? ")
 
-print("So, you're {0} old, {1} tall and {2} heavy.".format(
-    age, height, weight))
+print "So, you're %r old, %r tall and %r heavy." % (
+    age, height, weight)
+
+print "So, you're %s old, %s tall and %s heavy." % (
+    age, height, weight)
+
+#age = input("How old are you? ")
+#
+#height = input("How tall are you? ")
+#
+#weight = input("How much do you weigh? ")
+#
+#print("So, you're {0} old, {1} tall and {2} heavy.".format(
+#   age, height, weight))
