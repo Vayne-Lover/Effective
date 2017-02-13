9c6c28fd6fdb3d824720404b0ce99a6c7daedfe9
Vayne-Lover <406378362@qq.com>
2017-02-13 19:59:01 +0800

    Add exercise 14.

---
+++ Python/Learn Python The Hard Way/ex14.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+
+from sys import argv
+
+script, name=argv
+
+promot = ">: "
+
+print("Hi,{0}.I'm {1}".format(script, name))
+
+a=input(promot)
+
+b=input(promot)
+
+c=input(promot)
+
+print("The result is {0},{1},{2}".format(a,b,c))
+
