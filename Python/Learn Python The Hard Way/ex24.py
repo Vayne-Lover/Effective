e5894354e7cd97cd0b39e465212399c233467f11
Vayne-Lover <406378362@qq.com>
2017-02-25 20:35:44 +0800

    Add exercise 24.

---
+++ Python/Learn Python The Hard Way/ex24.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+
+print "Let's practice everything:"
+print 'You\'d need to \\ know \'bout escapes with \\ that do \n newlines and \t tabs.'
+
+poem = """
+\tThe lovely world\t
+with logic so firmly planted
+cannot discern \n the needs of love
+nor comprehend passion from intuition
+and requires an explanation
+\n\t\twhere there is none.
+"""
+
+print "--------------"
+print poem
+print "--------------"
