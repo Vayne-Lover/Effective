e798a1fc5eca7494d40396306fdff65e731b67b2
Vayne-Lover <406378362@qq.com>
2017-02-28 15:31:05 +0800

    Add exercise 27.

---
+++ Python/Learn Python The Hard Way/ex27.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+
+a = 1 and 0
+b = 1 or 3
+c = not a
+d = (a != b)
+e = (a == b)
+f = (2 >= 3)
+g = (1 <= 3)
+h = True or True and False
+i = False and True or True
+
+print(a,b,c)
+
+print(d,e,f)
+
+print(g,h,i)
