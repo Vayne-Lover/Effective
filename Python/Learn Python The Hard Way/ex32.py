d209f15bbccb1a44c939fa8315629b6236ae31c9
Vayne-Lover <406378362@qq.com>
2017-03-05 23:15:09 +0800

    Add exercise 32.

---
+++ Python/Learn Python The Hard Way/ex32.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+
+the_count = [1, 2, 3, 4, 5]
+
+fruits = ['apples', 'oranges', 'pears', 'apricots']
+
+change = [1, 'pennies', 2, 'dimes', 3, 'quarters']
+
+for i in the_count:
+  print(i)
+
+for j in fruits:
+  print(j)
+
+for k in change:
+  print(k)
+
+element=[]
+
+for i in range(6):
+  element.append(i)
+
+for e in element:
+  print(e)
