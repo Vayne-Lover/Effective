68e053211980a94eb7e404f14084c1389557c311
Vayne-Lover <406378362@qq.com>
2017-03-13 21:08:25 +0800

    Use dead function to replace print.

--- Python/Learn Python The Hard Way/ex35.py
+++ Python/Learn Python The Hard Way/ex35.py
@@ -2,6 +2,10 @@
 
 from sys import exit
 
+def dead(reason):
+  print(reason)
+  exit(0)
+
 def gold_room():
   print("You enter a room full of gold.")
   print("How much gold will you take?")
@@ -11,14 +15,14 @@ def gold_room():
   if answer.isdigit() :
     amount=int(answer)
   else:
-    print("Die !")
+    dead("Input a number!")
     exit(0)
 
   if amount <= 50 :
     print("Nice!")
     exit(0)
   else:
-    print("You die!")
+    dead("You die!")
     exit(0)
 
 if __name__ == "__main__" :
