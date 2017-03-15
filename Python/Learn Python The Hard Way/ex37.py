7d41974c8498c01be2de4d18d445d05a9ac7d065
Vayne-Lover <406378362@qq.com>
2017-03-15 21:16:03 +0800

    Add exercise 37.

---
+++ Python/Learn Python The Hard Way/ex37.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+
+print("The symbol contain five parts.")
+
+print("First part is keywords.")
+
+print("Second part is data types.")
+
+print("Third part is string escape sequences.")
+
+print("Fourth part is string formats.")
+
+print("Fifth part is operators.")
