4cfaf242da1cf44ee406e63fde3f8a5b33c7ed74
Vayne-Lover <406378362@qq.com>
2017-03-18 19:51:17 +0800

    Use * args to replace parameters.

--- Python/Effective Python/item18.py
+++ Python/Effective Python/item18.py
@@ -1,12 +1,16 @@
 # -*- codig: utf-8 -*-
 
-def log(mes,v):
+def log(mes,*v):
   if not v:
     print(mes)
   else:
     v_str=",".join(str(i) for i in v)
     print(mes,v_str)
 
-log("Hi!",[2,4])
+log("Hi!",2,4)
 
-log("Ha!",[])
+log("Ha!")
+
+a=[1,3,5]
+
+log("Ya!",*a)
