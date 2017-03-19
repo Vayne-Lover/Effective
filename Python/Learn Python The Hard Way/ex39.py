b93a0d83e01a45f40644c5ce4311e494603f786b
Vayne-Lover <406378362@qq.com>
2017-03-19 21:57:41 +0800

    Add exercise 39.

---
+++ Python/Learn Python The Hard Way/ex39.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+
+things=[1,2,3,4,5]
+
+print(things[1])
+
+things[1]=3
+
+print(things)
+
+stuff = {'name': 'Zed', 'age': 39, 'height': 6 * 12 + 2}
+
+print(stuff)
+
+stuff['city'] = "San Francisco"
+
+print(stuff)
+                                                                                                                                                             
+del stuff["age"]
+
+print(stuff)
