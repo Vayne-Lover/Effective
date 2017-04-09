57550ce45361a6afb888bd364121530fa74cfdb3
Vayne-Lover <406378362@qq.com>
2017-04-09 16:14:45 +0800

    Add class map.

--- Python/Learn Python The Hard Way/ex43.py
+++ Python/Learn Python The Hard Way/ex43.py
@@ -183,3 +183,24 @@ class Finished(Scene):
     def enter(self):
         print "You won! Good job."
         return 'finished'
+
+class Map:
+  
+    scenes = {
+        'central_corridor': CentralCorridor(),
+        'laser_weapon_armory': LaserWeaponArmory(),
+        'the_bridge': TheBridge(),
+        'escape_pod': EscapePod(),
+        'death': Death(),
+        'finished': Finished(),
+    }
+
+    def __init__(self, start_scene):
+        self.start_scene = start_scene
+
+    def next_scene(self, scene_name):
+        val = Map.scenes.get(scene_name)
+        return val
+
+    def opening_scene(self):
+        return self.next_scene(self.start_scene)
