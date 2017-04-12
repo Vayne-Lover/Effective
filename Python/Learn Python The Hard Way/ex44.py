7f56ef0b6e65c64b13f6ecb99d57b57cae9422f3
Vayne-Lover <406378362@qq.com>
2017-04-12 12:24:11 +0800

    Add override function.

--- Python/Learn Python The Hard Way/ex44.py
+++ Python/Learn Python The Hard Way/ex44.py
@@ -7,9 +7,23 @@ class Parent:
 class Child(Parent):
   pass
 
+class A:
+  def override(self):
+    print("A!")
+
+class B(A):
+  def override(self):
+    print("B!")
+
 if __name__=="__main__":
   dad=Parent()
   child=Child()
 
   dad.implicit()
   child.implicit()
+
+  a=A()
+  b=B()
+
+  a.override()
+  b.override()
