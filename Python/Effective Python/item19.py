121872c15d52e95815d5ea9745851dad388362f2
Vayne-Lover <406378362@qq.com>
2017-04-22 14:51:37 +0800

    Add flow_rate().

--- Python/Effective Python/item19.py
+++ Python/Effective Python/item19.py
@@ -3,8 +3,14 @@
 def remainder(number,divisor):
   return number % divisor
 
+def flow_rate(weight,time,period=1):
+  return weight/time*period
+
 if __name__=="__main__":
   print(remainder(20,7))
   print(remainder(20,divisor=7))
   print(remainder(number=20,divisor=7))
   print(remainder(divisor=7,number=20))
+
+  print(flow_rate(0.5,3))
+  print(flow_rate(6,3,100))
