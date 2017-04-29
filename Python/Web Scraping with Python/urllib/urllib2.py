978897f4aa7acd24541dbab8867a6b19a0cf62de
Vayne-Lover <406378362@qq.com>
2017-04-29 13:49:12 +0800

    Add part of urllib2 script.

---
+++ Python/Web Scraping with Python/urllib/urllib2.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+
+import urllib2
+
+def clear():
+    time.sleep(3)
+    OS=platform.system()
+    if (OS==u"Windows"):
+        os.system("cls")
+    else:
+        os.system("clear")
+
