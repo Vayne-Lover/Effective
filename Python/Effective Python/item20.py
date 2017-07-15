74d9152daddc8f3ac3c6319847c42c13e38885b2
Vayne-Lover <406378362@qq.com>
2017-07-15 23:02:29 +0800

    Add docs.

--- Python/Effective Python/item20.py
+++ Python/Effective Python/item20.py
@@ -4,6 +4,12 @@ from datetime import datetime
 from time import sleep
 
 def log(message, when = datetime.now()):
+    '''
+    Log a message with a timestamp.
+    :param message: a message
+    :param when: time
+    :return: None
+    '''
     print('{0} : {1} '.format(when, message))
 
 log('Hi there!')
