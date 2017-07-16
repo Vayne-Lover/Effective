34daa2b1d4ccb6416dbb13381ad47132bf0ad960
Vayne-Lover <406378362@qq.com>
2017-07-16 14:20:36 +0800

    Finish item 20.

--- Python/Effective Python/item20.py
+++ Python/Effective Python/item20.py
@@ -3,6 +3,8 @@
 from datetime import datetime
 from time import sleep
 
+import json
+
 def log(message, when = None):
     '''
     Log a message with a timestamp.
@@ -13,8 +15,25 @@ def log(message, when = None):
     when = datetime.now()
     print('{0} : {1} '.format(when, message))
 
-log('Hi there!')
+def decode(data, default = None):
+    if default is None:
+        default = {}
+    try:
+        return json.loads(data)
+    except ValueError:
+        return default
 
-sleep(1.5)
+if __name__ == "__main__":
+    foo = decode("bad data")
+    foo['stuff'] = 5
+    bar = decode("also bad")
+    bar['meet'] = 1
+    print(foo)
+    print(bar)
 
-log('Hi again!')
+
+#log('Hi there!')
+#sleep(1.5)
+#log('Hi again!')
+
+
